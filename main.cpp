92172858a0189f6a6d1abf6ea74b69e81f587675
gonzaloangaut <angaut13@gmail.com>
2026-02-26 00:07:09 -0300

    Fixed some errors

--- main.cpp
+++ main.cpp
@@ -288,40 +288,49 @@ public:
 int main()
 {
     // Declare the parameters of the simulation
-    int side = 16;
+    int side;
+    std::cout << "Ingrese el lado: ";
+    std::cin >> side;
     double J = 1.0;
     double B = 0.0;
     // Set the temperatures range
-    double T_start = 2.27;
-    double T_end = 2.27;
-    double dT = 0.1;
+    // List with variable resolution
+    std::vector<double> T_list;
+    
+    // High temperature
+    for (double T = 5.0; T >= 2.6; T -= 0.1) T_list.push_back(T);
+    
+    // Critic temperature
+    for (double T = 2.58; T >= 2.0 - 0.001; T -= 0.02) T_list.push_back(T);
+    
+    // Low temperature
+    for (double T = 1.9; T >= 1.0 - 0.001; T -= 0.1) T_list.push_back(T);
 
     // Create the network for the fist time to reserve memory
-    IsingModel model(side, J, B, T_start);
+    IsingModel model(side, J, B, T_list[0]);
     // model.printNetwork();
 
     // Get the number of sites
     int number_sites = model.getNumberSites();
 
     // Define the number of montecarlo steps
-    int MCS_termalizacion = 20000;
+    int MCS_termalizacion = 10000;
 
     // Create the name of the file to store the data for thermalization
-    std::string nombre_archivo = "data_thermalization_L=" + std::to_string(side) + ".csv";
+    // std::string nombre_archivo = "data_thermalization_L=" + std::to_string(side) + ".csv";
     // Open the file to save the data
-    std::ofstream archivo_term(nombre_archivo);
+    // std::ofstream archivo_term(nombre_archivo);
     // Write it
-    archivo_term << "T,MCS,Mag_per_spin,Energy_per_spin\n";
+    // archivo_term << "T,MCS,Mag_per_spin,Energy_per_spin\n";
 
     // Create the name of the file to store the data for the steady states
     std::string nombre_res = "results_L=" + std::to_string(side) + ".csv";
     // Open the file to save the data
     std::ofstream archivo_res(nombre_res);
     // Write it
-    archivo_res << "T,Mag_avg,Mag_avg2,Mag_avg4,Energy_avg,Susceptibility,Specific_heat\n";
-
+    archivo_res << "T,Mag_avg,Mag_avg2,Mag_avg4,Mag_abs,Energy_avg,Susceptibility,Specific_heat\n";
     // We see every temperature
-    for (double T = T_start; T >= T_end - 0.001; T -= dT)
+    for (double T : T_list)
     {
         // std::cout << "\n Simulando T = " << T << std::endl;
         // Update the temperature
@@ -339,28 +348,28 @@ int main()
                 model.metropolisStep();
             }
 
-            // Get the data
-            double m = model.getMagnetization();
-            double E = model.getEnergy(J, B);
-            // Get the energy for spin
-            double e = E / (double)number_sites;
+            // // Get the data
+            // double m = model.getMagnetization();
+            // double E = model.getEnergy(J, B);
+            // // Get the energy for spin
+            // double e = E / (double)number_sites;
 
-            // Save it
-            archivo_term << T << "," << t << "," << m << "," << e << "\n";
+            // // Save it
+            // archivo_term << T << "," << t << "," << m << "," << e << "\n";
         }
         // model.printNetwork();
 
         // Now we continue saving data after thermalization
-        int number_configurations = 1000;     // How many configurations we want
-        int MCS_decorrelacion = 20;      // Time between configurations
+        int number_configurations = 3000;     // How many configurations we want
+        int MCS_sample = 50;      // Time between configurations
 
-        double sum_m = 0.0, sum_m2 = 0.0, sum_m4 = 0.0;
+        double sum_m = 0.0, sum_m2 = 0.0, sum_m4 = 0.0, sum_m_abs = 0.0;
         double sum_e = 0.0, sum_e2 = 0.0;
 
         for (int m = 0; m < number_configurations; m++)
         {
             // Run the sistems for the time of decorrelation
-            for (int skip = 0; skip < MCS_decorrelacion; skip++) {
+            for (int skip = 0; skip < MCS_sample; skip++) {
                 // Run 1 MCS
                 for (int i = 0; i < number_sites; i++) {
                     model.metropolisStep();
@@ -368,13 +377,15 @@ int main()
             }
 
             // Get the instantaneous m and e
-            double m_inst = std::abs(model.getMagnetization()); 
+            double m_inst = model.getMagnetization(); 
+            double m_abs = std::abs(m_inst);
             double e_inst = model.getEnergy(J, B) / (double)number_sites;
 
             // Accumulate it
             sum_m  += m_inst;
             sum_m2 += m_inst * m_inst;
             sum_m4 += m_inst * m_inst * m_inst * m_inst;
+            sum_m_abs += m_abs;
             sum_e  += e_inst;
             sum_e2 += e_inst * e_inst;
         }
@@ -383,6 +394,7 @@ int main()
         double avg_m  = sum_m / number_configurations;
         double avg_m2 = sum_m2 / number_configurations;
         double avg_m4 = sum_m4 / number_configurations;
+        double avg_mabs = sum_m_abs / number_configurations;
         double avg_e  = sum_e / number_configurations;
         double avg_e2 = sum_e2 / number_configurations;
 
@@ -391,11 +403,11 @@ int main()
         double cv  = (number_sites / (T * T)) * (avg_e2 - (avg_e * avg_e));
 
         // Guardamos los promedios en el archivo de resultados
-        archivo_res << T << "," << avg_m << "," << avg_m2 << "," << avg_m4 << "," << avg_e << "," << chi << "," << cv << "\n";
+        archivo_res << T << "," << avg_m << "," << avg_m2 << "," << avg_m4 << "," << avg_mabs << "," << avg_e << "," << chi << "," << cv << "\n";
         
     }
     // Close the files
-    archivo_term.close();
+    // archivo_term.close();
     archivo_res.close();
 
     return 0;
